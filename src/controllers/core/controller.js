8b6179b2caf55867a9e1318d5311e36042b927d6
Lisa van Casteren <e.h.m.van.casteren@student.rug.nl>
2017-01-12 10:33:15 +0100

    Restore _updateMoney

--- src/controllers/core/controller.js
+++ src/controllers/core/controller.js
@@ -102,14 +102,15 @@ class Controller
     }
 
     /**
-         * Updates the current amount of money. Note that amount is added to GAME.model.config.money,
-         * so input negative amount to subtract!
-         *
-         * @param {float} amount - the amount to be added to the current amount of money
-         *
-         * @protected
-         */
-        _updateMoney(amount)
-        {
-            GAME.model.config.money = GAME.model.config.money + parseFloat(amount);
-            $("#money").html(GAME.model.config.money.toFixed(2));
+     * Updates the current amount of money. Note that amount is added to GAME.model.config.money,
+     * so input negative amount to subtract!
+     *
+     * @param {float} amount - the amount to be added to the current amount of money
+     *
+     * @protected
+     */
+    _updateMoney(amount)
+    {
+        GAME.model.config.money = GAME.model.config.money + parseFloat(amount);
+        $("#money").html(GAME.model.config.money.toFixed(2));
+    }
