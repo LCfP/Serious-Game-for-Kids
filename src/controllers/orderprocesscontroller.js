aca65ee08d97ab933a606eeac714265f80a33fd4
Rinnert <rjpolitiek@hotmail.com>
2017-12-19 08:19:24 +0300

    Start of structure

--- src/controllers/orderprocesscontroller.js
+++ src/controllers/orderprocesscontroller.js
@@ -17,16 +17,12 @@ export default class OrderProcessController extends Controller
     {
         this.order.products.forEach(product => {
             while (product.values.quantity) {
-                // see http://stackoverflow.com/a/2641374/4316405
-                GAME.model.warehouse.items.every(container => {
-                    const cases = {
-                        "FactoryOrder": container.addItem.bind(container),
-                        "CustomerOrder": container.removeItem.bind(container)
-                    };
 
-                    product.values.quantity = cases[this.order.constructor.name](product);
-                    return product.values.quantity;
-                });
+                if (product.values.isPerishable === true && this.order.constructor.name == "CustomerOrder") {
+                    this._perishableOrder(product);
+                } else {
+                    this._nonPerishableOrder(product);
+                }
             }
         });
 
@@ -34,4 +30,41 @@ export default class OrderProcessController extends Controller
             return true;
         }
     }
+
+    /**
+     * @private
+     */
+    _perishableOrder(product)
+    {
+        GAME.model.warehouse.items.forEach(container => {
+            //this._sortPerishables(container);
+
+            product.values.quantity = container.removeItem.bind(container)(product);
+            return product.values.quantity;
+        });
+    }
+
+    /**
+     * @private
+     */
+    _nonPerishableOrder(product)
+    {
+        // see http://stackoverflow.com/a/2641374/4316405
+        GAME.model.warehouse.items.every(container => {
+            const cases = {
+                "FactoryOrder": container.addItem.bind(container),
+                "CustomerOrder": container.removeItem.bind(container)
+            };
+
+            product.values.quantity = cases[this.order.constructor.name](product);
+            return product.values.quantity;
+        });
+    }
+
+    //Maybe move to container?
+    _sortPerishables(container)
+    {
+        container.items.forEach()
+    }
 }
+
