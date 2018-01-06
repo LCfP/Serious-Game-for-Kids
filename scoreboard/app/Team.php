89f50a171b7813a3fc30cb1fe3efbeb312bd1056
lukko <lucjwpol@gmail.com>
2018-01-06 18:43:11 +0100

    Make the team name mass assignable

--- scoreboard/app/Team.php
+++ scoreboard/app/Team.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
+    protected $fillable = [
+        'name'
+    ];
+
     public function room()
     {
         return $this->belongsTo(Room::class);
