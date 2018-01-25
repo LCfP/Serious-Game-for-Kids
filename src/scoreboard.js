ec918f1d9eead0173644b4465f62c47b44d5eb73
lukko <lucjwpol@gmail.com>
2018-01-25 16:41:16 +0100

    Create common js + css files

---
+++ src/scoreboard.js
@@ -0,0 +1,11 @@
+// Javascript
+import 'bootstrap/dist/js/bootstrap.min';
+
+// Stylesheets
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+$(document).ready(function ()
+{
+    console.log('scoreboard ready!');
+});
