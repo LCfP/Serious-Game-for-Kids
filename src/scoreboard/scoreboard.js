d1872dd0f83432cc43c7242af9fdd3eb27c5c558
lukko <lucjwpol@gmail.com>
2018-02-12 21:17:29 +0100

    move function out of document ready

--- src/scoreboard/scoreboard.js
+++ src/scoreboard/scoreboard.js
@@ -6,12 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './scoreboard.css';
 
 
+let scoreboardUrl = 'scoreboard.html';
+let scoreboardApiUrl = 'http://scoreboard.test';
+let timeBetweenUpdates = 30; // seconds
+
 $(document).ready(function ()
 {
-    let scoreboardUrl = 'scoreboard.html';
-    let scoreboardApiUrl = 'http://scoreboard.test';
     let roomName = getUrlParam('room');
-    let timeBetweenUpdates = 30; // seconds
 
     $('[id^="page-"]').hide();
 
@@ -36,79 +37,79 @@ $(document).ready(function ()
             window.location = scoreboardUrl + '?room=' + data.name;
         });
     });
+});
 
-    function getRoomInfo(name) {
-        $.get(scoreboardApiUrl + '/rooms/' + name).done(data => {
-            $('#room-name').text(data.name);
+function getRoomInfo(name) {
+    $.get(scoreboardApiUrl + '/rooms/' + name).done(data => {
+        $('#room-name').text(data.name);
 
+        getTeams(name);
+        setTimer();
+
+        setInterval(function () {
             getTeams(name);
             setTimer();
+        }, timeBetweenUpdates * 1000);
+    }).fail(error => {
+        window.location = scoreboardUrl;
+    });
+}
 
-            setInterval(function () {
-                getTeams(name);
-                setTimer();
-            }, timeBetweenUpdates * 1000);
-        }).fail(error => {
-            window.location = scoreboardUrl;
-        });
+function setTimer() {
+    let timeLeft = timeBetweenUpdates;
+
+    $('#scoreboard-timer').html(timeBetweenUpdates);
+
+    let x = setInterval(() => {
+        $('#scoreboard-timer').html(--timeLeft);
+
+        if (timeLeft <= 0) {
+            clearInterval(x);
+        }
+    }, 1 * 1000);
+}
+
+function getTeams(room) {
+    $.get(scoreboardApiUrl + '/rooms/' + room + '/teams').done(teams => {
+        updateTable(teams);
+    })
+}
+
+function calculateScore(team) {
+    return team.latest_score.money * team.latest_score.satisfaction / 100;
+}
+
+function updateTable(teams) {
+    $('#table-content').empty();
+
+    teams.sort(function (a, b) {
+        return calculateScore(b) - calculateScore(a);
+    });
+
+    for (var i = 0; i < teams.length; i++) {
+        let team = teams[i];
+            
+        var rowHtml = "<tr>";
+        rowHtml += "<td>" + (i+1) + "</td>";
+        rowHtml += "<td>" + team.name + "</td>";
+        rowHtml += "<td><b>" + calculateScore(team) + "</b></td>";
+        rowHtml += "<td>" + team.latest_score.money + "</td>";
+        rowHtml += "<td>" + team.latest_score.satisfaction + "&percnt;</td>";
+        rowHtml += "</tr>";
+
+        $('#table-content').append(rowHtml);
     }
+}
 
-    function setTimer() {
-        let timeLeft = timeBetweenUpdates;
+function getUrlParam(param) {
+    let params = window.location.search.substring(1);
+    params = params.split('&');
 
-        $('#scoreboard-timer').html(timeBetweenUpdates);
+    for (var i = 0; i < params.length; i++) {
+        let parameter = params[i].split('=');
 
-        let x = setInterval(() => {
-            $('#scoreboard-timer').html(--timeLeft);
-
-            if (timeLeft <= 0) {
-                clearInterval(x);
-            }
-        }, 1 * 1000);
-    }
-
-    function getTeams(room) {
-        $.get(scoreboardApiUrl + '/rooms/' + room + '/teams').done(teams => {
-            updateTable(teams);
-        })
-    }
-
-    function calculateScore(team) {
-        return team.latest_score.money * team.latest_score.satisfaction / 100;
-    }
-    
-    function updateTable(teams) {
-        $('#table-content').empty();
-
-        teams.sort(function (a, b) {
-            return calculateScore(b) - calculateScore(a);
-        });
-
-        for (var i = 0; i < teams.length; i++) {
-            let team = teams[i];
-                
-            var rowHtml = "<tr>";
-            rowHtml += "<td>" + (i+1) + "</td>";
-            rowHtml += "<td>" + team.name + "</td>";
-            rowHtml += "<td><b>" + calculateScore(team) + "</b></td>";
-            rowHtml += "<td>" + team.latest_score.money + "</td>";
-            rowHtml += "<td>" + team.latest_score.satisfaction + "&percnt;</td>";
-            rowHtml += "</tr>";
-
-            $('#table-content').append(rowHtml);
+        if (parameter[0] == param) {
+            return parameter[1];
         }
     }
-
-    function getUrlParam(param) {
-        let params = window.location.search.substring(1);
-        params = params.split('&');
-
-        for (var i = 0; i < params.length; i++) {
-            let parameter = params[i].split('=');
-
-            if (parameter[0] == param) {
-                return parameter[1];
-            }
-        }
-    }
-});
+}
